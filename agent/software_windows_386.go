d1e8caf02f543b7f562eb98e77827c4e4b11d1b3
wh1te909 <dcparsi@gmail.com>
2021-01-22 18:28:07 -0800

    remove unused func

--- agent/software_windows_386.go
+++ agent/software_windows_386.go
@@ -1,31 +1,10 @@
 package agent
 
 import (
-	"strings"
-
 	so "github.com/iamacarpet/go-win64api/shared"
 	wapf "github.com/wh1te909/go-win64api"
 	rmm "github.com/wh1te909/rmmagent/shared"
 )
-
-// GetProgramVersion loops through the registry for software
-// and if found, returns its version
-func (a *WindowsAgent) GetProgramVersion(name string) string {
-	sw, err := installedSoftwareList()
-	if err != nil {
-		a.Logger.Debugf("%s\r\n", err.Error())
-		return "0.0.0"
-	}
-
-	var lowerName string
-	for _, s := range sw {
-		lowerName = strings.ToLower(s.Name())
-		if strings.Contains(lowerName, name) {
-			return s.Version()
-		}
-	}
-	return "0.0.0"
-}
 
 func installedSoftwareList() ([]so.Software, error) {
 	sw32, err := wapf.GetSoftwareList(`SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall`, "X32")
--- agent/software_windows_amd64.go
+++ agent/software_windows_amd64.go
@@ -1,30 +1,9 @@
 package agent
 
 import (
-	"strings"
-
 	wapi "github.com/iamacarpet/go-win64api"
 	rmm "github.com/wh1te909/rmmagent/shared"
 )
-
-// GetProgramVersion loops through the registry for software
-// and if found, returns its version
-func (a *WindowsAgent) GetProgramVersion(name string) string {
-	sw, err := wapi.InstalledSoftwareList()
-	if err != nil {
-		a.Logger.Debugf("%s\r\n", err.Error())
-		return "0.0.0"
-	}
-
-	var lowerName string
-	for _, s := range sw {
-		lowerName = strings.ToLower(s.Name())
-		if strings.Contains(lowerName, name) {
-			return s.Version()
-		}
-	}
-	return "0.0.0"
-}
 
 func (a *WindowsAgent) GetInstalledSoftware() []rmm.SoftwareList {
 	ret := make([]rmm.SoftwareList, 0)
