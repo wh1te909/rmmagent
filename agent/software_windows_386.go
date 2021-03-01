633ac77cc69fe8eb8c31d668d5caf8dbbc3b37c5
wh1te909 <dcparsi@gmail.com>
2021-03-01 00:20:40 -0800

    make software install date match win control panel format

--- agent/software_windows_386.go
+++ agent/software_windows_386.go
@@ -1,6 +1,8 @@
 package agent
 
 import (
+	"fmt"
+
 	so "github.com/iamacarpet/go-win64api/shared"
 	wapf "github.com/wh1te909/go-win64api"
 	rmm "github.com/wh1te909/rmmagent/shared"
@@ -24,11 +26,12 @@ func (a *WindowsAgent) GetInstalledSoftware() []rmm.SoftwareList {
 	}
 
 	for _, s := range sw {
+		t := s.InstallDate
 		ret = append(ret, rmm.SoftwareList{
 			Name:        s.Name(),
 			Version:     s.Version(),
 			Publisher:   s.Publisher,
-			InstallDate: s.InstallDate.String(),
+			InstallDate: fmt.Sprintf("%02d/%02d/%d", t.Month(), t.Day(), t.Year()),
 			Size:        ByteCountSI(s.EstimatedSize * 1024),
 			Source:      s.InstallSource,
 			Location:    s.InstallLocation,
--- agent/software_windows_amd64.go
+++ agent/software_windows_amd64.go
@@ -1,6 +1,8 @@
 package agent
 
 import (
+	"fmt"
+
 	wapi "github.com/iamacarpet/go-win64api"
 	rmm "github.com/wh1te909/rmmagent/shared"
 )
@@ -14,11 +16,12 @@ func (a *WindowsAgent) GetInstalledSoftware() []rmm.SoftwareList {
 	}
 
 	for _, s := range sw {
+		t := s.InstallDate
 		ret = append(ret, rmm.SoftwareList{
 			Name:        s.Name(),
 			Version:     s.Version(),
 			Publisher:   s.Publisher,
-			InstallDate: s.InstallDate.String(),
+			InstallDate: fmt.Sprintf("%02d/%02d/%d", t.Month(), t.Day(), t.Year()),
 			Size:        ByteCountSI(s.EstimatedSize * 1024),
 			Source:      s.InstallSource,
 			Location:    s.InstallLocation,
