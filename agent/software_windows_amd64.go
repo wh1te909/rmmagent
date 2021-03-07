f24727cc2d107626b93c254d0ab1d71ae2e19ddc
wh1te909 <dcparsi@gmail.com>
2021-03-06 16:37:07 -0800

    change install date format wh1te909/tacticalrmm#288

--- agent/software_windows_amd64.go
+++ agent/software_windows_amd64.go
@@ -21,7 +21,7 @@ func (a *WindowsAgent) GetInstalledSoftware() []rmm.SoftwareList {
 			Name:        s.Name(),
 			Version:     s.Version(),
 			Publisher:   s.Publisher,
-			InstallDate: fmt.Sprintf("%02d/%02d/%d", t.Month(), t.Day(), t.Year()),
+			InstallDate: fmt.Sprintf("%02d-%d-%02d", t.Year(), t.Month(), t.Day()),
 			Size:        ByteCountSI(s.EstimatedSize * 1024),
 			Source:      s.InstallSource,
 			Location:    s.InstallLocation,
